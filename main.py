ab370e29b9448f6783dbec3c6a292f4eadee8745
Abhishek Shinde <139879930+areusnow@users.noreply.github.com>
2025-08-16 10:34:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import csv
+
+income = 0.0
+expense = 0.0
+
+with open('transactions.csv', 'r') as file:
+    reader = DictReader(file)
+
+    for row in reader:
+        amount = float(row['Amount'])
+        
+        if amount > 0:
+            income += amount
+        else:
+            expense -= amount
+        
+print("--- Financial Summary ---")
+print(f"Total income = ${income:.2f}")
+print(f"Total expense = ${abs(expense):.2f}")
+print(f"Net Balance = ${(income + expense):.2f}")
